d197c10a896c21797d1624dc07e2d1626371d998
elliesea <la.belle.mer.517@gmail.com>
2023-01-14 22:31:51 +0900

    Tripコントローラー,Tripモデル作成

---
+++ database/migrations/2023_01_14_211939_create_trips_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();  //$table->データの型('カラム名')
+            $table->integer('user_id');
+            $table->string('title', 50);
+            $table->text('body');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TripController;  //外部にあるTripControllerクラスをインポート。
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/trips', [TripController::class, 'index']);   
