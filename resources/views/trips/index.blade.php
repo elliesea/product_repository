1f6902184b33c764c330bd2fa5476bca1b838ad1
elliesea <la.belle.mer.517@gmail.com>
2023-01-16 01:10:44 +0900

    バリデーションの実装

--- resources/views/trips/index.blade.php
+++ resources/views/trips/index.blade.php
@@ -2,16 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <title>Trips</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
+        <a href='/trips/create'>create</a>
         <div class='trips'>
             @foreach ($trips as $trip)
                 <div class='trip'>
-                    <h2 class='title'>{{ $trip->title }}</h2>
+                    <h2 class='title'>
+                        <a href="/trips/{{ $trip->id }}">{{ $trip->title }}</a>
+                    </h2>
                     <p class='body'>{{ $trip->body }}</p>
                 </div>
             @endforeach
